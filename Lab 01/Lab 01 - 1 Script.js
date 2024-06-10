d3e0b5200a05505d29c6686b81e8afc7f9b7fb0b
Nisarg Lo <nisarg31305@gmail.com>
2024-06-10 18:03:07 +0530

    10-6-24

---
+++ Lab 01/Lab 01 - 1 Script.js
@@ -0,0 +1,18 @@
+a = prompt("Enter number 1 :");
+b = prompt("Enter number 2 :");
+c = prompt("Enter any operation from '+', '-', '*', '/', '%' :");
+if (c == "+") {
+     alert("Sum = " + (parseInt(a) + parseInt(b)));
+}
+else if (c == "-") {
+     alert("Substract = " + (parseInt(a) - parseInt(b)));
+}
+else if (c == "*") {
+     alert("Multiply = " + (parseInt(a) * parseInt(b)));
+}
+else if (c == "/") {
+     alert("Divide = " + (parseInt(a) - parseInt(b)));
+}
+else if (c == "%") {
+     alert("Modulus = " + (parseInt(a) - parseInt(b)));
+}

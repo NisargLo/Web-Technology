9071b5c6a9f16296c789d7270bdc3e5d7b49ac63
Nisarg Lo <nisarg31305@gmail.com>
2024-06-11 23:53:39 +0530

    11-6-24

--- Lab 02/Lab2-1-A Script.js
+++ Lab 02/Lab2-1-A Script.js
@@ -1,3 +1,14 @@
 // WAP in JavaScript to print the factors of given number.
 
 n=prompt("Enter number :");
+document.write("<label>Factors : </label>")
+for(i=1;i<=n;i++){
+     if(n%i==0){
+          if(i!=n){
+               document.write("<label>"+i+", </label>");
+          }
+          else{
+               document.write("<label>"+i+"</label>");
+          }
+     }
+}

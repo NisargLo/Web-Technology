1f7affce46d3cb027ec9b9432ba6690fe08bbc66
Nisarg Lo <nisarg31305@gmail.com>
2024-06-19 17:23:37 +0530

    19-6-24

--- Lab 01/Lab1-5 Script.js
+++ Lab 01/Lab1-5 Script.js
@@ -1,18 +1,18 @@
 // WAP in JavaScript to check whether the given number is palindrome or not.
 
-n=prompt("Enter number :");
-l=0;
-h=n.length-1;
-f=1;
-while(l<=h){
-     if(n.charAt(l)!=n.charAt(h)){
-          f=0;
+n = prompt("Enter number :");
+l = 0;
+h = n.length - 1;
+f = 1;
+while (l <= h) {
+     if (n.charAt(l) != n.charAt(h)) {
+          f = 0;
           alert("Not Palindrome");
           break;
      }
      l++;
      h--;
 }
-if(f==1){
+if (f == 1) {
      alert("Palindrome");
 }

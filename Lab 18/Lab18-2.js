dd96179693832505697a4c9924edfdab411f565f
Nisarg Lo <nisarg31305@gmail.com>
2024-08-20 19:20:36 +0530

    20-8-24

--- Lab 18/Lab18-2.js
+++ Lab 18/Lab18-2.js
@@ -1,6 +1,7 @@
 // Create a webapp with 5 pages like about, contact etc.. using “http” core module in NodeJS.
 
 const http = require("http");
+require('dotenv').config({ path: './Lab WT/.env' });
 
 const server = http.createServer((req, res) => {
      if(req.url === "/home"){
@@ -20,8 +21,11 @@ const server = http.createServer((req, res) => {
      }
 });
 
-const port = 3100;
-
-server.listen(3100,()=>{
-     console.log("Server is listening on port 3100");
+const port = process.env.PORT;
+server.listen(port, (err) => {
+     if (err) {
+          console.log(`Error in listening on port ${port}`);
+          return;
+     }
+     console.log(`App is listening on port ${port}`);
 });
--- Lab 19/Lab19-1.js
+++ Lab 19/Lab19-1.js
@@ -1,7 +1,7 @@
 // Create a hello world webapp using ExpressJS
 
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: './Lab WT/.env' });
 
 const app = express();
 
